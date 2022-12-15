3bf192497afa4762d66f8f9f92137dc9244372d4
Ahmed Rabie <ahmed.rabie.mahmoud.96@gmail.com>
2022-12-15 13:39:59 +0200

    empty parameter request handled

--- index.js
+++ index.js
@@ -18,15 +18,25 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/", (req,res) => {
+  res.json({ unix: Date.now(), utc: Date()});
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+
+app.get("/api/:date?", function (req, res) {
+
+  let dateFromUser = req.params.date;
+
+  let date = new Date(dateFromUser);
+  
+  
+  res.json({unix: date.valueOf(), utc: ""});
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
